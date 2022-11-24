1cd866d6e5fd7aae2c4de24e41f0c111ef59c664
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2022-11-24 06:31:57 +0530

    Update listofcoins.js

--- listofcoins.js
+++ listofcoins.js
@@ -13,7 +13,7 @@ function createRow({ id, name, symbol, image, current_price, market_cap, market_
 						  `;
   }
   async function getUsersData() {
-	const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false`);
+	const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=250&page=1&sparkline=false`);
 	const userList = await res.json();
 	console.log(userList);
   
