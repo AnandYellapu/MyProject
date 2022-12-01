faffec4c9b85344c3f8ff068febfb4f7e3820eec
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2022-12-01 19:09:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,3 +82,13 @@ var swiper = new Swiper(".home-slider", {
  function heart (icon) {
   $(icon).toggleClass('fas far');
 }
+
+// -------------email validation-------------------
+
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return (true);
+  }
+  alert("You have entered an invalid email address!");
+  return (false);
+}
