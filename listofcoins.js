34935dc08020ea29102f24b488a87ec589d11002
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2022-12-01 08:10:21 +0530

    Update listofcoins.js

--- listofcoins.js
+++ listofcoins.js
@@ -1,6 +1,6 @@
 function createRow({ id, name, symbol, image, current_price, market_cap, market_cap_rank,total_volume,total_supply,circulating_supply  }) {
 	document.querySelector(".userlist").innerHTML += `                        
-	<td class="far fa-star"></td>
+	<td class="far fa-star" onClick="star(this)"></td>
 	<td>${id}</td>
 	<td><img src=${image}  alt="image1" width=20px height=20px style="text-align:center;"></td>
 	<td>${symbol}</td>
@@ -45,3 +45,8 @@ function createRow({ id, name, symbol, image, current_price, market_cap, market_
 	firstTenUsers.map((element) => createRow(element));
   }
   getUsersData();
+
+
+  function star (icon) {
+  $(icon).toggleClass('fas far');
+}
